ab73eaac8def7235304441897e8a20581adf729d
kk5190 <kk5190krrish@gmail.com>
2022-03-14 22:46:50 +0530

    Authentication : Connecting the User with their Data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
@@ -22,11 +25,17 @@ export class AuthService {
       userData.password
     );
 
-    await this.usersCollection.add({
+    if (!userCred.user) {
+      throw new Error("User can't be found");
+    }
+    await this.usersCollection.doc(userCred.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+    await userCred.user.updateProfile({
+      displayName: userData.name,
     });
   }
 }
