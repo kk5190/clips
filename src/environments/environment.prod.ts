f88d1d4b7b6c6271995562038ef95034cef7c27c
kk5190 <kk5190krrish@gmail.com>
2022-03-14 15:03:24 +0530

    configure angualr fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-bUgo-eGDX-ASyRw6SFYELsh9EXGSja4",
+    authDomain: "clips-a476a.firebaseapp.com",
+    projectId: "clips-a476a",
+    storageBucket: "clips-a476a.appspot.com",
+    messagingSenderId: "316210867614",
+    appId: "1:316210867614:web:079b6e62856ebc9c1c7a8f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC-bUgo-eGDX-ASyRw6SFYELsh9EXGSja4",
+    authDomain: "clips-a476a.firebaseapp.com",
+    projectId: "clips-a476a",
+    storageBucket: "clips-a476a.appspot.com",
+    messagingSenderId: "316210867614",
+    appId: "1:316210867614:web:079b6e62856ebc9c1c7a8f"
+  }
 };
 
 /*
